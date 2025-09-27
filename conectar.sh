a2ee95e699a0a69b39ef314d216be29527653908
Guilherme <guilhermeb.redentor@gmail.com>
2025-09-27 11:03:04 -0300

    feat: Add script de conexão e teste

---
+++ conectar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Configurando kubectl para o cluster challenge-cluster..."
+aws eks update-kubeconfig --region us-east-1 --name challenge-cluster
+echo "Pronto! Testando a conexão:"
+kubectl get nodes
