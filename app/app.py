b2acc1ef17caf340fc1ead64f8763a3a304b4668
Guilherme <guilhermeb.redentor@gmail.com>
2025-09-26 22:16:12 -0300

    feat: Estrutura inicial do projeto para o desafio LuizaLabs

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+# Importa a biblioteca de métricas
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+# Aplica o "wrapper" de métricas no seu app.
+# Isso cria automaticamente o endpoint /metrics para você!
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello():
-    return "Hello, Magalu iPET! - by Guilherme Barrios"
+    return "Hello, Magalu iPET! - Guilherme Barrios"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
