5153511cd5d8ed28f442fc16da3d5f2ed9788c46
Guilherme <guilherme.redentor@gmail.com>
2025-10-02 15:07:27 -0300

    teste.3

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,9 +1,9 @@
 # infra/variables.tf
 
-variable "meu_ip_acesso" {
-  description = "O IP público da minha máquina para liberar o acesso ao cluster EKS"
-  type        = string
-}
+#variable "meu_ip_acesso" {
+  #description = "O IP público da minha máquina para liberar o acesso ao cluster EKS"
+  #type        = string
+#}
 
 # Provider e região
 variable "aws_region" {
